e7845997e0df2d4720a3c7f2b3c0b6be22e08a93
maste_000 <maste_000@128.4.121.150>
2015-02-26 16:20:52 -0500

    here it is

---
+++ src/MainPackage/MyInteger.java
@@ -0,0 +1,101 @@
+package MainPackage;
+
+public class MyInteger {
+
+	public int value(int value) {
+
+		int MyInteger = value;
+		{
+		}
+	}
+
+	public int getValue();
+
+	{
+		return MyInteger;
+	}
+
+	public boolean isEven(int MyInteger){
+	
+		if (MyInteger%2 ==0)
+			return true;
+			else
+				return false;
+						
+	public boolean isOdd(){
+		
+		if (MyInteger%2==1)
+			return true;
+			else
+				return false;
+	}
+	public boolean isPrime(){
+		if (MyInteger%2==0)
+			return false;
+		for(int i=3;i*i<=MyInteger;i+=2){
+			if(MyInteger%i==0)
+				return false;
+		
+		}
+		return true;		
+	}
+				
+	}
+
+	public static double isEven(int) {
+		if (MyInteger % 2 == 0)
+			return true;
+		else
+			return false;
+	}
+
+	public static double isOdd(int) {
+		if (MyInteger % 2 == 1)
+			return true;
+		else
+			return false;
+
+	}
+	public static double isPrime(int){
+		if (MyInteger%2==0)
+			return false;
+		for(int i=3;i*i<=MyInteger;i+=2){
+			if(MyInteger%i==0)
+				return false;
+		}
+		return true;
+	}
+	public static double isEven(MyInteger){
+		if (MyInteger%2==0)
+			return true;
+			else
+				return false;
+	}
+	public static double isOdd(Myinteger){
+		if(MyInteger%2==1)
+			return true;
+		else
+			return false
+					
+	}
+	public static double isPrime(MyIntegers){
+		if(MyIntegers%2==0)
+			return false
+		for(int i=3;i*i<=MyInteger;i+=2){
+			if(MyInteger%i==0)
+				return false;
+		}
+		return true
+	}
+	public equals(int){
+	
+	}
+	public equals(MyInteger){
+		
+	}
+	public static parseInt(char[]){
+		
+	}
+	public static parseInt(String)
+	
+}
